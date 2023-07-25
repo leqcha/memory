a0689db14966130e4bc2f15beb918ce1ebf31b11
leqcha <86303498+leqcha@users.noreply.github.com>
2023-07-25 14:23:05 +0200

    Add files via upload
    Memory game for child

--- main.js
+++ main.js
@@ -7,25 +7,41 @@ var tabJeu = [
     [0, 0, 0, 0]
 ];
 
+// var tabResultat = [
+//     [1,4,3,4],
+//     [1,2,3,2],
+//     [7,8,6,5],
+//     [8,7,5,6]
+// ]
+var tabResultat = genereTableauAleatoire();
+
+var oldSelection = [];
+var nbAffiche = 0;
+var ready = true;
+
 afficherTableau();
 
 function afficherTableau() {
     var txt = "";
 
-    for (let i = 0; i < tabJeu.length; i++) {
+    for (var i = 0; i < tabJeu.length; i++) {
         txt += "<div>";
-        for (let j = 0; j < tabJeu[i].length; j++) {
+        for (var j = 0; j < tabJeu[i].length; j++) {
             if (tabJeu[i][j] === 0) {
-                txt += "<button class = 'btn btn-primary m-2'>Afficher</button>"
+                txt += "<button class='btn btn-primary m-2' onClick='verif(\"" + i + "-" + j + "\")'>Tourner</button>";
             } else {
                 txt += "<img src='" + getImage(tabJeu[i][j]) + "' class='m-2'>";
-
             }
-
         }
         txt += "</div>";
     }
+
     divResultat.innerHTML = txt;
+
+    // Vérifier si toutes les figures sont retournées
+    if (toutesFiguresRetournees()) {
+        afficherMessageBravo();
+    }
 }
 
 function getImage(valeur) {
@@ -47,7 +63,79 @@ function getImage(valeur) {
             break;
         case 8: imgTxt += "pig.png";
             break;
-        default: console.log("Y a un truc qui marche pas");
+        default: console.log("cas non pris en compte")
     }
     return imgTxt;
+}
+
+function verif(bouton) {
+    if (ready) {
+        nbAffiche++;
+
+        var ligne = bouton.substr(0, 1);
+        var colonne = bouton.substr(2, 1);
+
+        tabJeu[ligne][colonne] = tabResultat[ligne][colonne];
+        afficherTableau();
+
+        if (nbAffiche > 1) {
+            ready = false;
+            setTimeout(() => {
+                //verification
+                if (tabJeu[ligne][colonne] !== tabJeu[oldSelection[0]][oldSelection[1]]) {
+                    tabJeu[ligne][colonne] = 0;
+                    tabJeu[oldSelection[0]][oldSelection[1]] = 0;
+                }
+                afficherTableau();
+                ready = true;
+                nbAffiche = 0;
+                oldSelection = [ligne, colonne];
+            }, 1000)
+        } else {
+            oldSelection = [ligne, colonne];
+            console.log("Bien joué !");
+        }
+    }
+}
+
+function genereTableauAleatoire() {
+    var tab = [];
+    var nbImagePosition = [0, 0, 0, 0, 0, 0, 0, 0];
+
+    for (var i = 0; i < 4; i++) {
+        var ligne = [];
+        for (var j = 0; j < 4; j++) {
+            var fin = false;
+            while (!fin) {
+                var randomImage = Math.floor(Math.random() * 8);
+                if (nbImagePosition[randomImage] < 2) {
+                    ligne.push(randomImage + 1);
+                    nbImagePosition[randomImage]++;
+                    fin = true;
+                }
+            }
+        }
+        tab.push(ligne);
+    }
+    return tab;
+}
+function toutesFiguresRetournees() {
+    // Vérifier si toutes les figures sont retournées
+    for (let i = 0; i < tabJeu.length; i++) {
+        for (let j = 0; j < tabJeu[i].length; j++) {
+            if (tabJeu[i][j] === 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function afficherMessageBravo() {
+    // Afficher le message "Bravo Lucien" avec une animation sympa ici.
+    // Par exemple, vous pouvez créer un élément de texte et lui ajouter des classes d'animation.
+    var messageBravo = document.createElement("div");
+    messageBravo.textContent = "Bravo Lucien!";
+    messageBravo.classList.add("message-bravo", "animate__animated", "animate__bounceInDown");
+    divResultat.appendChild(messageBravo);
 }
